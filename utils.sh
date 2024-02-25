92d8d6e2c053847aad7e8ecca00db46813222357
lyhengLeang <ammheng110@gmail.com>
2024-02-25 15:34:10 +0700

    first commit message

---
+++ utils.sh
@@ -0,0 +1,8 @@
+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
+
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://lyhengLeang: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@github.com/lyhengLeang/test.git
+git push -u origin main
